b4ec69748c6b50f970648c145a2cf4a0293fc3c1
VIJAYADASS <124162965+VIJAYADASS@users.noreply.github.com>
2024-08-01 20:05:08 +0530

    Create sub App component using both class and function components

---
+++ src/assets/Types of Component/FCApp.jsx
@@ -0,0 +1,13 @@
+import Table from "./ClassComponent";
+import FunctionComponent from "./FunctionComponent ";
+
+function FCApp() {
+  return (
+    <div>
+      <FunctionComponent />
+      <Table />
+    </div>
+  );
+}
+
+export default FCApp;
