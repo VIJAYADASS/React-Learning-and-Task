959cea3080a2b5f7629d08d24daaf2e025d90b63
VIJAYADASS <124162965+VIJAYADASS@users.noreply.github.com>
2024-08-01 20:24:10 +0530

    Class components should display a counter that can be incremented by clicking a button.

---
+++ src/assets/Types of Component/ClassComponentTask.jsx
@@ -0,0 +1,27 @@
+import { Component } from "react";
+
+class ClassCounter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
+  }
+
+  incrementCount = () => {
+    this.setState((prevState) => ({
+      count: prevState.count + 1,
+    }));
+  };
+
+  render() {
+    return (
+      <div>
+        <p>Class Counter: {this.state.count}</p>
+        <button onClick={this.incrementCount}>Increment</button>
+      </div>
+    );
+  }
+}
+
+export default ClassCounter;
