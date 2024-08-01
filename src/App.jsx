d2b63215e1d9736fbc49d4d9e761cb8c56d881de
VIJAYADASS <124162965+VIJAYADASS@users.noreply.github.com>
2024-08-01 20:03:10 +0530

    Initialize App component for React learning task

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,11 @@
-import Propsapp from "./assets/Prop and JSX/Propsapp";
-import Table from "./assets/Types of Component/Fun & Cls Component ";
-import EffectsApp from "./assets/UseEffects/EffectsApp";
 import "./App.css";
-import UseStateApp from "./assets/UseState/UseStateApp";
-import UseContextApp from "./assets/UseContext/UseContextApp";
+import FCApp from "./assets/Types of Component/FCApp";
 
 function App() {
   return (
     <>
-      <h2 className="h2"> Func and Cls Components : Table</h2>
-      <Table />
-      <br />
-      <h2 className="h2"> Props</h2>
-      <Propsapp />
-      <br />
-      <h2 className="h2">UseState Hook</h2>
-      <UseStateApp />
-      <br />
-      <h2 className="h2">UseEffects Hook</h2>
-      <EffectsApp />
-      <br />
-      <h2 className="h2">UseContext Hook</h2>
-      <UseContextApp />
+      <h2 className="h2">Function and Class Components </h2>
+      <FCApp />
     </>
   );
 }
