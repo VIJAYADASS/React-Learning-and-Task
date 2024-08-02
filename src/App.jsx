a6ea687abf618936ea18a78c94893a4278f8d11a
VIJAYADASS <124162965+VIJAYADASS@users.noreply.github.com>
2024-08-02 20:32:54 +0530

    Add props to component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import "./App.css";
-import FCApp from "./assets/Types of Component/FCApp";
+import FCApp from "./assets/Day-01 Types of Components/FCApp";
+import ParentComponent from "./assets/Day-02  Props/ParentComponentTask";
+import Propsapp from "./assets/Day-02  Props/Propsapp";
+// import FCApp from "";
 
 function App() {
   return (
     <>
       <h2 className="h2">Function and Class Components </h2>
       <FCApp />
+      <h2 className="h2">Props </h2>
+      <Propsapp />
+      <h2 className="h2">Props Task</h2>
+      <ParentComponent />
     </>
   );
 }
