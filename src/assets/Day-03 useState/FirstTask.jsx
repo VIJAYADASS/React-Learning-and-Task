559bc9c41bd22d50225a6badc3ee1e21eece62b7
VIJAYADASS <124162965+VIJAYADASS@users.noreply.github.com>
2024-08-03 23:17:01 +0530

    Add tasks for using useState hooks in counter component: increment, decrement, resetAdd tasks for using useState hooks in counter component: increment, decrement, reset

---
+++ src/assets/Day-03 useState/FirstTask.jsx
@@ -0,0 +1,36 @@
+// Task 1
+
+import { useState } from "react";
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
+  const reset = () => {
+    setCount(0);
+  };
+
+  return (
+    <div>
+      <p>Current number:{count} </p>
+      <button className="firstTask" onClick={increment}>
+        Increment
+      </button>
+      <button className="firstTask" onClick={decrement}>
+        Decrement
+      </button>
+      <button className="firstTask" onClick={reset}>
+        Reset
+      </button>
+    </div>
+  );
+};
+
+export default Counter;
