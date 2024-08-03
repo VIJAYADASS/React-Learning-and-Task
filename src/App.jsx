ae0a8b2dc2bdb46b35e3cf84f3e14a4c39b92688
VIJAYADASS <124162965+VIJAYADASS@users.noreply.github.com>
2024-08-03 23:12:42 +0530

    Add useState hook for state management

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import FCApp from "./assets/Day-01 Types of Components/FCApp";
 import ParentComponent from "./assets/Day-02  Props/ParentComponentTask";
 import Propsapp from "./assets/Day-02  Props/Propsapp";
+import UseStateApp from "./assets/Day-03 useState/UseStateApp";
 // import FCApp from "";
 
 function App() {
@@ -13,6 +14,8 @@ function App() {
       <Propsapp />
       <h2 className="h2">Props Task</h2>
       <ParentComponent />
+      <h2 className="h2">useState Hook</h2>
+      <UseStateApp />
     </>
   );
 }
