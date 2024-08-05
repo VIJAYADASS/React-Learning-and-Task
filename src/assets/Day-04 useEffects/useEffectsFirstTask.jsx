81681c4c553686a4bd8cf2facd83459920bcb6a8
VIJAYADASS <124162965+VIJAYADASS@users.noreply.github.com>
2024-08-05 20:06:21 +0530

    Create a simple React component that fetches data from an API when the component mounts.

---
+++ src/assets/Day-04 useEffects/useEffectsFirstTask.jsx
@@ -0,0 +1,29 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+function FetchData() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        setUsers(res.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default FetchData;
